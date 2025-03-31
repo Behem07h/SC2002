b9ed907bb7baa1ec6bfdeaabf3b5fee379a4ede6
Behem07h <behem07h119@gmail.com>
2025-03-31 14:13:23 +0800

    user manager and user interface template

--- src/main/java/org/example/UserManager.java
+++ src/main/java/org/example/UserManager.java
@@ -1,8 +1,12 @@
 package org.example;
 
 public class UserManager {
-
+    User[] usr_db;
     public void authenticate() {
         //ask for user login
+    }
+
+    public void add_user() {
+        //ask for user details to add to usr_db;
     }
 }

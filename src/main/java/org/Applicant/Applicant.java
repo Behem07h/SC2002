5352f8fab2fcbb320567a06da73f1947d931e364
yuqin882 <CAIY0049@e.ntu.edu.sg>
2025-04-09 16:57:23 +0800

    display user successfully

--- src/main/java/org/Applicant/Applicant.java
+++ src/main/java/org/Applicant/Applicant.java
@@ -3,7 +3,6 @@ package org.Applicant;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 public class Applicant implements user {
     private String userID;
@@ -11,7 +10,7 @@ public class Applicant implements user {
     private String password;  // Plain-text password
     private String maritalStatus;
     private int age;
-    private PermissionLevel perms;  // Default to PermissionLevel.NONE
+    private PermissionLevel perms;  // Default permission level
     private List<Enquiry> enquiries = new ArrayList<>();
 
     // Constructor
@@ -21,8 +20,9 @@ public class Applicant implements user {
         this.password = password;  // Plain-text password
         this.maritalStatus = maritalStatus;
         this.age = age;
-        this.perms = perms;  // Default permission level
+        this.perms = perms;
     }
+
     public void createEnquiry(String projectID, String content) {
         Enquiry enquiry = new Enquiry(projectID, getUserID(), content);
         enquiries.add(enquiry);
@@ -69,12 +69,12 @@ public class Applicant implements user {
 
     @Override
     public String getPassword() {
-        return password;  // Plain-text password
+        return password;
     }
 
     @Override
     public void setPassword(String password) {
-        this.password = password;  // Set plain-text password
+        this.password = password;
     }
 
     @Override
@@ -103,7 +103,6 @@ public class Applicant implements user {
         System.out.println("Applicant [" + username + "] is acting on: " + something);
     }
 
-
     @Override
     public boolean changePassword(String oldPassword, String newPassword) {
         if (this.password.equals(oldPassword)) {  // Plain-text password comparison
@@ -113,14 +112,21 @@ public class Applicant implements user {
         return false;
     }
 
-    // Enum for Permission Levels (can still be used but always set to NONE if no data is provided)
+    @Override
+    public String toString() {
+        return "Applicant{username='" + username + "', userID='" + userID + "'}";
+    }
+
+    // Enum for Permission Levels
     public enum PermissionLevel {
-        READ,  // Read permission
-        WRITE, // Write permission
-        ADMIN, // Administrator privileges
-        NONE   // No special permissions (default value)
+        READ,   // Read permission
+        WRITE,  // Write permission
+        ADMIN,  // Administrator privileges
+        NONE    // No special permissions (default value)
     }
 }
+
+
 
 
 

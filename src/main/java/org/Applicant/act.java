51a73ce44e65b5f4c633c962867ea4807fbc4adb
yuqin882 <CAIY0049@e.ntu.edu.sg>
2025-04-11 12:01:53 +0800

    act interface done

---
+++ src/main/java/org/Applicant/act.java
@@ -0,0 +1,14 @@
+package org.Applicant;
+
+public interface act{
+    /**
+     * Perform a view action.
+     */
+    void view();
+
+    /**
+     * Perform a submit action.
+     */
+    void submit();
+}
+

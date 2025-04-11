30a864196cbb205990a34934cc425cd7f04ea8cd
sleepyjudge <minrae04@gmail.com>
2025-04-11 14:21:38 +0800

    registration

--- src/main/java/org/action/registration/Registration.java
+++ src/main/java/org/action/registration/Registration.java
@@ -1,5 +1,10 @@
-package org.action;
+package org.action.registration;
 
-public interface registration {
-
+public interface Registration {
+    void approveRegistration();
+    void rejectRegistration();
+    void registerProject();
+    void viewRegistrationProject();
 }
+
+

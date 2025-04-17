9e4560cc7cfa9fbd2a7d0f952d47903f15ea7de6
Behem07h <behem07h119@gmail.com>
2025-04-17 14:17:30 +0800

    act fn draft

--- src/main/java/org/Applicant/user.java
+++ src/main/java/org/Applicant/user.java
@@ -1,7 +1,7 @@
 package org.Applicant;
 
 public interface user {
-    
+
     String getUserID();
 
     void setUserID(String userID);
@@ -25,7 +25,44 @@ public interface user {
 
     boolean changePassword(String oldPassword, String newPassword);
 
-    String[] act(String action); //wrapper fn that handles user inputs for all actions
+    default String[] act(String action) {
+        String[] output = new String[] {};
+        switch (action){
+            //enquiry methods
+            case "submit-enquiry":
+                break;
+            case "delete-enquiry":
+                break;
+            case "edit-enquiry":
+                break;
+            case "reply-enquiry":
+                break;
+            //application methods
+            case "view-applications":
+                break;
+            case "add-application":
+                break;
+            case "retrieve-application":
+                break;
+            case "withdraw-application":
+                break;
+            case "approve-application":
+                break;
+            case "reject-application":
+                break;
+            //project methods
+            case "view-projects":
+                break;
+            case "search-projects":
+                break;
+            case "filter-projects":
+                break;
+            case "delete-projects":
+                break;
+            default:
+                return output;
+        }
+    } //wrapper fn that handles user inputs for all actions
 }
 
 
